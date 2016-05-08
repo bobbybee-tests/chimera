c2118408099051d4b90439e83ec6e37ae77e4dad
Alyssa Rosenzweig <alyssa@backtick.town>
2016-05-08 16:04:36 -0400

    Stub breakout for chimera

--- chimera.rkt
+++ chimera.rkt
@@ -6,6 +6,28 @@
 (require racket)
 
 (define (chimera-entry program)
-  (pretty-print program))
+  (chimera-compile (first (first program)) '() (hash 'distance 0)))
 
-(chimera-entry (first (rest (read))))
+(define (chimera-compile block emission ctx)
+  (if (empty? block)
+    (list emission ctx)
+    (let ([line (chimera-line (first block) ctx)])
+      (chimera-compile (rest block)
+                       (append (first line) emission)
+                       (second line)))))
+
+(define (chimera-line line ctx)
+  (if (equal? (first line) "=")
+    (let ([expression (chimera-expression line ctx)])
+      (list (cons (list "setLine:ofList:to:"
+                        (list "readVariable" "sp") ; TODO: calculate differences
+                        "memory"
+                        (first expression))
+                  (second expression))
+            (third expression)))
+    (display "Unknown command\n")))
+
+(define (chimera-expression value ctx)
+  (list 42 '() ctx))
+
+(pretty-print (chimera-entry (first (rest (read)))))
